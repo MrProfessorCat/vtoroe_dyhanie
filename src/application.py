7743acc5ca304f0f8a9e49e2e78bcbf0f064fdcc
Andrey <professor.cat@inbox>
2023-09-17 16:25:42 +0300

    test devops 1

--- src/application.py
+++ src/application.py
@@ -9,8 +9,8 @@ from bot.handlers import greeting
 def main() -> None:
     """Start the bot"""
     app = Application.builder().token(settings.telegram_token).build()
-    app.add_handler(CommandHandler('start', greeting))
-    app.add_handler(CommandHandler('help', help))
+    app.add_handler(CommandHandler("start", greeting))
+    app.add_handler(CommandHandler("help", help))
     app.add_handler(CommandHandler("stop", stop))
 
     app.run_polling()
